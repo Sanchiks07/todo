6637db33e0dd6ba4a072303a35b39be1884789bf
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-03-03 10:50:20 +0200

    izveidoju skatus ar maršrutiem

---
+++ resources/views/watch.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Watch</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="navbar">
+        <ul>
+            <li><a href="/apple">Apple Home</a></li>
+            <li><a href="/store">Store</a></li>
+            <li><a href="/mac">Mac</a></li>
+            <li><a href="/iphone">iPhone</a></li>
+            <li><a href="/watch">Watch</a></li>
+            <li><a href="/airpods">AirPods</a></li>
+            <li><a href="/support">Support</a></li>
+        </ul>
+    </div>
+
+    <h1>Watch</h1>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/why', function () {
+    return view('why');
+});
+
+Route::get('/apple', function () {
+    return view('apple');
+});
+
+Route::get('/store', function () {
+    return view('store');
+});
+
+Route::get('/mac', function () {
+    return view('mac');
+});
+
+Route::get('/iphone', function () {
+    return view('iphone');
+});
+
+Route::get('/watch', function () {
+    return view('watch');
+});
+
+Route::get('/airpods', function () {
+    return view('airpods');
+});
+
+Route::get('/support', function () {
+    return view('support');
+});
