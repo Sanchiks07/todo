f0dfb3d79f9151c6cfddfab49c01177a22772ed7
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-03-05 12:48:27 +0200

    nam nam diary

--- resources/views/diaries/index.blade.php
+++ resources/views/diaries/index.blade.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Veicamie uzdevumi</title>
+    <title>Dienagrāmata</title>
 </head>
 <body>
-    <h1>Visi veicamie uzdevumi</h1>
+    <h1>Dienasrāmatas ieraksti</h1>
     <ul>
         @foreach ($diaries as $diary)
             <li>{{ $diary->title }}</li>
