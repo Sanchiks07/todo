e5dd37293f4a374cb002fd757b882b612bf1922b
ipb23.s.mitniece <ipb23.s.mitniece@pjvt.local>
2025-03-06 13:59:50 +0200

    nam nam style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,8 @@
     <x-slot:title>
         Welcome
     </x-slot:title>
-    <h1>Heloings, Laravel!</h1>
-    <ul>
-        <li><a href="/apple">Apple Store</a></li>
+        <h1>Heloings, Laravel!</h1>
+        <ul>
+            <li><a href="/apple">Apple Store</a></li>
+        </ul>
 </x-layout>
